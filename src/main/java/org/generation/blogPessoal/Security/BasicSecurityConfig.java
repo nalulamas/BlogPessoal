a173608ed6be10190a99aec3fb03cd45d20129b3
Paulo <analu.lamas@yahoo.com.br>
2022-03-14 17:52:13 -0300

    @nalulamas[task: issues 0.0.4]

--- src/main/java/org/generation/blogPessoal/Security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blogPessoal/Security/BasicSecurityConfig.java
@@ -15,35 +15,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Override
-	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
-		
-		auth.userDetailsService(userDetailsService);
-		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
-		.authorities("ROLE_ADMIN");
-
-	}	
-	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests()
-				.antMatchers(HttpMethod.POST,"/users/register").permitAll()
-				.antMatchers(HttpMethod.POST,"/users/login").permitAll()
+		.antMatchers(HttpMethod.POST, "/users/register").permitAll()
+				.antMatchers(HttpMethod.POST, "/users/login").permitAll()
 				.anyRequest().authenticated()
 				.and().httpBasic()
 				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and().cors()
 				.and().csrf().disable();
-				
+
 	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root")).authorities("ADMIN");
+
+	}
+	
 }
